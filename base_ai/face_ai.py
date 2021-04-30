8a3dd4dd5952fb9345c6f337a21b0122373b3ed3
Chen Yipeng <chenyipeng@CN1514001193M.local>
2021-04-30 10:36:20 +0800

    add mouth_opened_detection.py

--- base_ai/face_ai.py
+++ base_ai/face_ai.py
@@ -45,8 +45,29 @@ class FaceMesh(object):
                 self.draw_engine.draw_landmarks(image=image, landmark_list=face_landmarks, connections=mp.solutions.face_mesh.FACE_CONNECTIONS, landmark_drawing_spec=drawing_spec, connection_drawing_spec=drawing_spec)
         return image
 
-    def mouth_opened_detection(self):
-        pass
+    def mouth_opened_detection(self, results, image_shape, threshold=0.2):
+        # Clockwise
+        mouth_feature_index = [78, 82, 312, 308, 317, 87]
+
+        # 只选取第一个人脸
+        face_landmarks = []
+        if results.multi_face_landmarks:
+            landmarks = results.multi_face_landmarks[0]
+            x = [landmark.x for landmark in landmarks.landmark]
+            y = [landmark.y for landmark in landmarks.landmark]
+            face_landmarks = np.transpose(np.stack((y, x))) * image_shape[:2]
+
+            # left eye
+            mouth_feature_location = face_landmarks[mouth_feature_index]
+            mouth_horizontal_distance = self.calculation_distance(mouth_feature_location[0], mouth_feature_location[3])
+            mouth_vertical_distance = (self.calculation_distance(mouth_feature_location[1], mouth_feature_location[5]) + 
+                self.calculation_distance(mouth_feature_location[2], mouth_feature_location[4])) /2
+            mouth_distance_ratio = mouth_vertical_distance / mouth_horizontal_distance
+        
+            if mouth_distance_ratio > threshold:
+                return True  
+
+        return False      
 
     def eyes_closed_detection(self, results, image_shape, threshold=0.2):
         # Clockwise
---
+++ examples/mouth_opened_detection.py
@@ -0,0 +1,34 @@
+import cv2
+import sys
+sys.path.append("..")
+from base_ai.face_ai import FaceMesh
+
+cap = cv2.VideoCapture(0)
+face_mesh = FaceMesh()
+last_mouth_opened_flag = False
+mouth_opened_count = 0
+
+while cap.isOpened():
+  
+    success, image = cap.read()
+    if not success:
+        print("Ignoring empty camera frame.")
+        continue
+
+    image = cv2.flip(image, 1)
+    results = face_mesh.inference(image)
+    image = face_mesh.draw(image, results)
+    
+    cur_mouth_opened_flag = face_mesh.mouth_opened_detection(results, image.shape, threshold=0.15)
+    if cur_mouth_opened_flag and not last_mouth_opened_flag:
+        mouth_opened_count += 1
+    last_mouth_opened_flag = cur_mouth_opened_flag
+
+    cv2.putText(image, 'mouth opended:{}'.format(cur_mouth_opened_flag), (50,50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 1)
+    cv2.putText(image, 'mouth opended count:{}'.format(mouth_opened_count), (50,100), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 1)
+    
+    cv2.imshow('Face Mesh', image)
+    if cv2.waitKey(5) & 0xFF == 27:
+        break
+
+cap.release()
